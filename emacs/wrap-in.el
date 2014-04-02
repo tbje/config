7c23335e80bb2783564a02493d53ddcfbe3439bc
Trond Bjerkestrand <trond@groosker.com>
2014-04-02 08:41:48 +0200

    Adding addId.

--- emacs/wrap-in.el
+++ emacs/wrap-in.el
@@ -20,8 +20,22 @@
   (wrap-in beg end "${" "}")
 )
 
+
 (defun wrap-in-string (beg end)
   "wrap some marked area in an Option[txt]"
   (interactive "r")
   (wrap-in beg end "\"" "\"")
 )
+
+(defun addId (beg end)
+  "wrap some marked area in an Option[txt]"
+  (interactive "r")
+  (let* ((id (buffer-substring-no-properties beg end))
+         (name (concat "val " id " = Id(\"" id "\")")))
+    (kill-new name)
+    (sbt-send (concat name "\n"))))
+
+
+
+
+
