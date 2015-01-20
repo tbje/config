351ab8a6380a959ba3c566d8e094a1a9a4101640
Trond Sandnes Bjerkestrand <trond@groosker.com>
2015-01-20 21:54:42 +0100

    Adding a su-find-file for bookmark+ open.

--- emacs/su-edit.el
+++ emacs/su-edit.el
@@ -1,7 +1,14 @@
 (require 'tramp)
 
 (defun su-edit ()
+  "edit as su - jumping to same position"
   (interactive)
   (let ((position (point)))
     (find-file (concat "/sudo::" (buffer-file-name)))
     (goto-char position)))
+
+(defun su-find-file (f)
+  "nice to use with bookmark+, i.e. (file-handler . su-find-file) will open the file as su"
+  (with-current-buffer
+      (find-file f)
+    (su-edit)))
