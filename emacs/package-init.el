73d69fa3b9c2507554e5f371c00da92cd396d320
Trond Sandnes Bjerkestrand <trond@groosker.com>
2015-01-27 13:03:34 +0100

    Use 'APPEND.

--- emacs/package-init.el
+++ emacs/package-init.el
@@ -1,5 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") 'APPEND)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") 'APPEND)
 (package-initialize)
 
